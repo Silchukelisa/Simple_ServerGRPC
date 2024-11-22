e12dbacd0ffd78e7b3a1d9e9e4d63b4f5a493d31
Silchukelisa <silchuk.liza14@gmail.com>
2024-11-22 22:55:32 +0300

    Initial commit

--- src/main/java/org/tyt/App.java
+++ src/main/java/org/tyt/App.java
@@ -1,13 +1,20 @@
 package org.tyt;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new GreetingServiceImpl())
+                .build();
+        server.start();
+        System.out.println("Server started");
+        server.awaitTermination();//чтоб не останавливалось, пока не остановим самостоятельно
     }
 }
--- src/main/java/org/tyt/GreetingServiceImpl.java
+++ src/main/java/org/tyt/GreetingServiceImpl.java
@@ -1,2 +1,35 @@
-package org.tyt;public class GreetingServiceImpl {
+package org.tyt;
+
+import io.grpc.stub.StreamObserver;
+import org.tyt.grpc.GreetingServiceGrpc;
+import org.tyt.grpc.GreetingServiceOuterClass;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request,
+                         StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        //отправляем поток данных
+        /*for(int i = 0; i < 10000; i++){
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse
+                    .newBuilder()
+                    .setGreeting("Hello from server, " + request.getName())
+                    .build();
+            responseObserver.onNext(response);
+        }*/
+
+        //отправляем один ответ
+        System.out.println(request);
+        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse
+                .newBuilder()
+                .setGreeting("Hello from server, " + request.getName())
+                .build();
+        responseObserver.onNext(response);
+
+        responseObserver.onCompleted(); //означает что мы больше не будем пересылать данные
+    }
 }
